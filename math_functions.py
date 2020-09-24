41123978d5f5d98978ef22860d082ac9b7a23fa3
evalynledezma <evalynledezmacode@gmail.com>
2020-09-24 15:59:49 -0600

    first commit

---
+++ math_functions.py
@@ -0,0 +1,4 @@
+def add_two_numbers(num_one, num_two):
+  return num_one + num_two
+def subtract_two_numbers(num_one, num_two):
+  return num_one - num_two
---
+++ one_function.py
@@ -0,0 +1,3 @@
+from math_functions import add_two_numbers 
+
+print(add_two_numbers(1,2))
